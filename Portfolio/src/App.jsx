86f951e96822d4e6f6650fd8f9c4b1d4baf97429
QzyKuma <quezjn24@gmail.com>
2024-10-31 20:43:21 -0400

    adding initial code

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+
+function App() {
+
+
+  return (
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+         <Footer />
+        </div>
+      </Router>
+  );
+}
+
+export default App
