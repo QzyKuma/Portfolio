d5478053959dbbbb096744c628d31b086dd58ecf
QzyKuma <quezjn24@gmail.com>
2024-10-31 23:40:51 -0400

    improved visuals

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import styled from 'styled-components'
 
+
+const Nav = styled.nav`
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        padding: 1rem 2rem;
+        background-color: #333;
+        color: #fff;
+`;
+
+const NavLink = styled(Link)`
+    color: #fff;
+    text-decoration: none;
+    margin: 0 1rem;
+    font-size: 1.1rem;
+    transition: color 0.3s;
+
+    &:hover {
+        color: #00d8ff;
+    }
+`;
 
 export default function Navbar() {
         return (
